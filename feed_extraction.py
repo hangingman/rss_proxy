d44d46c97858ff4c540739a92c6273049ddf97f7
hiroyuki nagata <newserver002@gmail.com>
2021-03-25 13:05:33 +0900

    Add from/to arguments

--- feed_extraction.py
+++ feed_extraction.py
@@ -24,11 +24,11 @@ def main(args: argparse.Namespace):
     proxy: ProxyHandler = proxy_auth(config)
 
     # 日付指定(from)の設定
-    target_date = yesterday()
+    target_date_from = yesterday()
     if args.from_date:
-        target_date = args.from_date
+        target_date_from = args.from_date
 
-    post_to_slack(target_url, proxy, webhook_url, target_date)
+    post_to_slack(target_url, proxy, webhook_url, target_date_from, args.to_date)
 
 
 def load_config(path: Optional[str] = None) -> dict:
@@ -49,21 +49,27 @@ def proxy_auth(config: dict) -> ProxyHandler:
     return proxy
 
 
-def post_to_slack(target_url, proxy, webhook_url, target_date):
-    attachments = feed_of_the_day(target_url, proxy, target_date)
+def post_to_slack(target_url: str,
+                  proxy: ProxyHandler,
+                  webhook_url: str, target_date_from: datetime, target_date_to: datetime):
+
+    attachments = feed_of_the_day(target_url, proxy, target_date_from, target_date_to)
+    dt_fmt = '%Y年%m月%d日 %H:%M:%S'
     if not attachments:
-        text = f'【{target_date}に掲示された記事はありません】'
+        text = f"【{target_date_from.strftime(dt_fmt)}〜{target_date_to.strftime(dt_fmt)}に掲示された記事はありません】"
     else:
-        text = f'【{target_date}に掲示された記事一覧】'
+        text = f"【{target_date_from.strftime(dt_fmt)}〜{target_date_to.strftime(dt_fmt)}に掲示された記事一覧】"
     requests.post(
         webhook_url,
         data=json.dumps({
             'text': text,
             'attachments': attachments,
-            'link_names': 1}))
+            'link_names': 1
+        })
+    )
 
 
-def feed_of_the_day(target_url: str, proxy: ProxyHandler, target_date: datetime):
+def feed_of_the_day(target_url: str, proxy: ProxyHandler, target_date_from: datetime, target_date_to: datetime):
     feed = feedparser.parse(target_url, handlers=[proxy])
     opener = build_opener(proxy)
     install_opener(opener)
@@ -76,7 +82,7 @@ def feed_of_the_day(target_url: str, proxy: ProxyHandler, target_date: datetime)
         rss_date: datetime = parse(rss_date_str).\
             astimezone(timezone(timedelta(hours=9), 'JST'))
 
-        if target_date.date() == rss_date.date():
+        if target_date_from <= rss_date <= target_date_to:
             # RSSのpublishedの日付が対象日付であれば
             attachments += [{
                 'title': entry['title'],
@@ -111,10 +117,13 @@ if __name__ == '__main__':
           (2) デフォルトでスクリプト実行時の前日に公開された記事のタイトル・リンク・本文をSlackに投稿 (Incoming WebHooksを利用)
         """
     )
-    parser.add_argument('--from-date', required=False, default=None, help='RSS取得したい対象日時(from) yyyy-mm-dd')
-    parser.add_argument('--to-date', required=False, default=None, help='RSS取得したい対象日時(to) yyyy-mm-dd')
+    tz = timezone(timedelta(hours=9), 'JST')
+    parser.add_argument('--from-date', required=False, default=None, help='RSS取得したい対象日時(from) yyyy/MM/dd HH:mm:ss')
+    parser.add_argument('--to-date', required=False, default=datetime.now(tz=tz), help='RSS取得したい対象日時(to) yyyy/MM/dd HH:mm:ss')
     parsed_args = parser.parse_args()
     if parsed_args.from_date:
-        parsed_args.from_date = datetime.strptime(parsed_args.from_date, '%Y-%m-%d')
+        parsed_args.from_date = parse(parsed_args.from_date.__str__()).astimezone(tz)
+    if parsed_args.from_date:
+        parsed_args.to_date = parse(parsed_args.to_date.__str__()).astimezone(tz)
 
     main(parsed_args)
